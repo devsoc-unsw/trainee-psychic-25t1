ec66a480aff7489c75656d9bdeff5f44e569ff9b
Hayden Ho <z5589170@ad.unsw.edu.au>
2025-05-15 16:41:52 +1000

    added basic navbar to home page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,6 +1,18 @@
+"use client"
+
+import Navbar from "./Navbar";
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(() => {
+    // somehow check if logged in
+  }, [])
+
   return (
     <div>
+      <Navbar/>
       <h1>Home</h1>
     </div>
   );
