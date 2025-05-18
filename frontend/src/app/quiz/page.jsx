dff9f2c0bfe353840678fb992f19ff971ee759d3
Andy <andy.ruan2005@gmail.com>
2025-05-18 11:44:00 +1000

    uploaded pictures of the map

--- frontend/src/app/quiz/page.jsx
+++ frontend/src/app/quiz/page.jsx
@@ -1,45 +1,31 @@
 
 "use client"
 import React from 'react';
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import useMousePosition from "./useMousePosition";
-export default function QuizPage() {
-    const {x,y} = useMousePosition();
-    
-    const [showShape, setShowShape] = React.useState(false)
-    const [coords, setCoords] = React.useState({x, y});
+import Quiz from './quiz';
+import locations from "./locations.json"
 
-    function getCoords(event) {
-        const rect = event.currentTarget.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
-        setShowShape(true)
-        setCoords({x, y})
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
     }
+}
 
+shuffle(locations)
+
+
+
+export default function QuizPage() {
     return (
-            <div className="flex h-1/2 w-6/7 mx-auto">
-                <div onClick={getCoords}>
-                        <TransformWrapper>
-                            <TransformComponent>
-                                <img src="/images/kensomap.gif" alt="Kensomap" className="w-full h-full object-fill" />
-                                        {showShape && <div className="bg-red-500 w-5 h-5 absolute" style={{ left: `${coords.x}px`, top: `${coords.y}px`, transform: 'translate(-50%, -50%)' }}>.</div>}
-                            </TransformComponent>
-                    </TransformWrapper>
-                </div>
-                <div className="divider divider-horizontal"></div>
-                <div className="flex flex-col">
-                    <div className='w-[450px] h-[460px]'>
-                        <img className='w-full h-full object-cover' src={"/images/unsw_locations/ainsworth.jpg"}></img>
-                    </div>
-                    <div className='mx-auto mt-auto justify-start'>
-                        <button className="btn btn-primary w-1/2 mb-5">Lock in</button>
-                        <p>Score: 0 | High Score: 0</p>
-                    </div>
-                </div>
-            </div>
-   
-
+        <Quiz location={locations}/>
     )
-
 }
