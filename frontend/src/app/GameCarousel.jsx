6f82c3223e3a7de34651967aa9180307d20270e9
Guan Li <guanLi@student.unsw.edu.au>
2025-05-18 16:50:11 +1000

    added basic button functionality and routing to carousel component

--- frontend/src/app/GameCarousel.jsx
+++ frontend/src/app/GameCarousel.jsx
@@ -1,32 +1,48 @@
 "use client";
 
+import { useRouter } from 'next/navigation';
 import Carousel from "@/components/ui/carousel";
 export default function GameCarousel() {
+  const router = useRouter();
+
   const slideData = [
     {
       title: "Mystic Mountains",
-      button: "Explore Component",
+      button: "Play Game",
       src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/game1"
     },
     {
       title: "Urban Dreams",
-      button: "Explore Component",
+      button: "Play Game",
       src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/game2"
     },
     {
       title: "Neon Nights",
-      button: "Explore Component",
+      button: "Play Game",
       src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/game3"
     },
     {
       title: "Desert Whispers",
-      button: "Explore Component",
+      button: "Play Game",
       src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/game4"
     },
   ];
+
+  const buttonClick = (slide) => {
+    alert("routing to: " + slide.path);
+    router.push(slide.path);
+  }
+
   return (
     <div className="relative overflow-hidden w-full h-full py-20">
-      <Carousel slides={slideData} />
+      <Carousel 
+        slides={slideData} 
+        onButtonClick={(slide) => buttonClick(slide)}
+      />
     </div>
   );
 }
