e0cf5db14d03f8ab7870c0f5205600e4f73cf6c7
Andy <andy.ruan2005@gmail.com>
2025-05-20 18:43:15 +1000

    made buttons change colour when clicked

--- frontend/src/app/minesweeper/page.jsx
+++ frontend/src/app/minesweeper/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from "react";
 
 function createBoard() {
   const rows = 10;
@@ -11,7 +12,7 @@ function createBoard() {
   for (let i = 0; i < rows; i++) {
     myArray[i] = [];
     for (let j = 0; j < cols; j++) {
-      myArray[i][j] = 0; 
+      myArray[i][j] = {'value': 0, 'revealed': false}; 
     }
   }
 
@@ -34,7 +35,7 @@ function createBoard() {
 
   for (const coords of mines) {
     const [x, y] = coords;
-    myArray[x][y] = 1;
+    myArray[x][y] = {'value': 1, 'revealed': false}; 
   }
 
   // debugging
@@ -42,70 +43,65 @@ function createBoard() {
   return myArray;
 }
 
-function displayBoard(board) {
+function displayBoard(board, handleClick) {
   const theBoard = [];
 
   function checkAdjacent(i, j) {
     let numMines = 0;
     // check i > 0
-    if (i > 0 && board[i - 1][j] === 1) {
+    if (i > 0 && board[i - 1][j].value === 1) {
       numMines++;
     } 
     // check j > 0
-    if (j > 0 && board[i][j - 1] === 1) {
+    if (j > 0 && board[i][j - 1].value === 1) {
       numMines++;
     } 
     // check that its not within the maxbound
-    if (i < 9 && board[i + 1][j] === 1) {
+    if (i < 9 && board[i + 1][j].value === 1) {
       numMines++;
     } 
     // check y coord is not within the max bound.
-    if (j < 9 && board[i][j + 1] === 1) {
+    if (j < 9 && board[i][j + 1].value === 1) {
       numMines++;
     }
 
     // check diagonals
     // top left diagonal
-    if (i > 0 && j > 0 && board[i-1][j-1] === 1) {
+    if (i > 0 && j > 0 && board[i-1][j-1].value === 1) {
       numMines++;
     }
 
     // top right diagonal
-    if (i < 9 && j > 0 && board[i+1][j-1] === 1) {
+    if (i < 9 && j > 0 && board[i+1][j-1].value === 1) {
       numMines++;
     }
 
     // bottom left diagonal
-    if (i > 0 && j < 9 && board[i-1][j+1] === 1) {
+    if (i > 0 && j < 9 && board[i-1][j+1].value === 1) {
       numMines++;
     }
 
     // bottom right diagonal
-    if (i < 9 && j < 9 && board[i+1][j+1] === 1) {
+    if (i < 9 && j < 9 && board[i+1][j+1].value === 1) {
       numMines++;
     }
 
     return numMines;
   }
   
-  function handleClick(event) {
-    const buttonElement = event.target;
-
-    const buttonText = buttonElement.textContent;
-    console.log(buttonText);
-    const clickedId = event.currentTarget.id;
-    console.log(`Button with ID: ${clickedId} was clicked.`);
-  }
-
-  
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const uniqueKey = `${i}-${j}`;
       const cellId = `cell-${i}-${j}`;
-      if (board[i][j] === 1) {
+
+      if (board[i][j].value === 1) {
         theBoard.push(<div key={uniqueKey} id={cellId} className="btn btn-warning">MINE</div>);
       } else {
-        theBoard.push(<div key={uniqueKey} id={cellId} onClick={handleClick} className="btn btn-success">{checkAdjacent(i, j)}</div>);
+        if (!board[i][j].revealed) {
+          theBoard.push(<div key={uniqueKey} id={cellId} onClick={() => handleClick(i, j)} className="btn btn-success">{checkAdjacent(i, j)}</div>);
+        } else {
+          theBoard.push(<div key={uniqueKey} id={cellId} onClick={() => handleClick(i, j)} className="btn btn-neutral">{checkAdjacent(i, j)}</div>);
+        }
       }
     }
   }
@@ -116,16 +112,22 @@ function displayBoard(board) {
 
 export default function MineSweeperPage() {
 
-  const board = createBoard();
-  // console.log(board);
-  const theBoard = displayBoard(board);
+  const [board, setBoard] = useState(createBoard());
+
+  function handleClick(row, col) {
+    const newBoard = JSON.parse(JSON.stringify(board)); 
+    newBoard[row][col].revealed = true; 
+    setBoard(newBoard);
+  }
+
+  const boardUI = displayBoard(board, handleClick);
 
   return (
     <>
       <div
         className="grid grid-cols-10 w-1/2 h-1/2 mx-auto"
       >
-        {theBoard}
+        {boardUI}
       </div>
     </>
   );
