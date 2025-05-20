39b99cd908556a802401b19e2a33f7663f0b2225
Guan Li <guanLi@student.unsw.edu.au>
2025-05-20 21:09:57 +1000

    snake route from main

--- frontend/src/app/GameCarousel.jsx
+++ frontend/src/app/GameCarousel.jsx
@@ -13,10 +13,10 @@ export default function GameCarousel() {
       path: "/wordle"
     },
     {
-      title: "Urban Dreams",
+      title: "Snake",
       button: "Play Game",
       src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      path: "/game2"
+      path: "/snake"
     },
     {
       title: "Neon Nights",
