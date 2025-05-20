644ea3ffb8b799d94a8c22d3c826a306a31dfac4
Andy <andy.ruan2005@gmail.com>
2025-05-20 20:05:31 +1000

    implemented bfs to check all adjacent grids if u click a zero

--- frontend/src/app/minesweeper/page.jsx
+++ frontend/src/app/minesweeper/page.jsx
@@ -12,7 +12,12 @@ function createBoard() {
   for (let i = 0; i < rows; i++) {
     myArray[i] = [];
     for (let j = 0; j < cols; j++) {
-      myArray[i][j] = {'value': 0, 'revealed': false}; 
+      myArray[i][j] = {
+        'value': 0,
+        'revealed': false,
+        'i': i,
+        'j': j,
+        'numMines': 0}; 
     }
   }
 
@@ -38,6 +43,52 @@ function createBoard() {
     myArray[x][y] = {'value': 1, 'revealed': false}; 
   }
 
+  function countAdjacentMines(i, j) {
+    let numMines = 0;
+    if (i > 0 && myArray[i - 1][j].value === 1) {
+      numMines++;
+    } 
+    // check j > 0
+    if (j > 0 && myArray[i][j - 1].value === 1) {
+      numMines++;
+    } 
+    // check that its not within the maxbound
+    if (i < 9 && myArray[i + 1][j].value === 1) {
+      numMines++;
+    } 
+    // check y coord is not within the max bound.
+    if (j < 9 && myArray[i][j + 1].value === 1) {
+      numMines++;
+    }
+
+    // check diagonals
+    // top left diagonal
+    if (i > 0 && j > 0 && myArray[i-1][j-1].value === 1) {
+      numMines++;
+    }
+
+    // top right diagonal
+    if (i < 9 && j > 0 && myArray[i+1][j-1].value === 1) {
+      numMines++;
+    }
+
+    // bottom left diagonal
+    if (i > 0 && j < 9 && myArray[i-1][j+1].value === 1) {
+      numMines++;
+    }
+
+    // bottom right diagonal
+    if (i < 9 && j < 9 && myArray[i+1][j+1].value === 1) {
+      numMines++;
+    }
+    return numMines;
+  }
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      myArray[i][j].numMines = countAdjacentMines(i, j);
+    }
+  }
   // debugging
   // console.log(myArray);
   return myArray;
@@ -46,48 +97,7 @@ function createBoard() {
 function displayBoard(board, handleClick) {
   const theBoard = [];
 
-  function checkAdjacent(i, j) {
-    let numMines = 0;
-    // check i > 0
-    if (i > 0 && board[i - 1][j].value === 1) {
-      numMines++;
-    } 
-    // check j > 0
-    if (j > 0 && board[i][j - 1].value === 1) {
-      numMines++;
-    } 
-    // check that its not within the maxbound
-    if (i < 9 && board[i + 1][j].value === 1) {
-      numMines++;
-    } 
-    // check y coord is not within the max bound.
-    if (j < 9 && board[i][j + 1].value === 1) {
-      numMines++;
-    }
-
-    // check diagonals
-    // top left diagonal
-    if (i > 0 && j > 0 && board[i-1][j-1].value === 1) {
-      numMines++;
-    }
-
-    // top right diagonal
-    if (i < 9 && j > 0 && board[i+1][j-1].value === 1) {
-      numMines++;
-    }
-
-    // bottom left diagonal
-    if (i > 0 && j < 9 && board[i-1][j+1].value === 1) {
-      numMines++;
-    }
-
-    // bottom right diagonal
-    if (i < 9 && j < 9 && board[i+1][j+1].value === 1) {
-      numMines++;
-    }
-
-    return numMines;
-  }
+ 
   
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
@@ -98,9 +108,9 @@ function displayBoard(board, handleClick) {
         theBoard.push(<div key={uniqueKey} id={cellId} className="btn btn-warning">MINE</div>);
       } else {
         if (!board[i][j].revealed) {
-          theBoard.push(<div key={uniqueKey} id={cellId} onClick={() => handleClick(i, j)} className="btn btn-success">{checkAdjacent(i, j)}</div>);
+          theBoard.push(<div key={uniqueKey} id={cellId} onClick={() => handleClick(i, j)} className="btn btn-success">{board[i][j].numMines}</div>);
         } else {
-          theBoard.push(<div key={uniqueKey} id={cellId} onClick={() => handleClick(i, j)} className="btn btn-neutral">{checkAdjacent(i, j)}</div>);
+          theBoard.push(<div key={uniqueKey} id={cellId} onClick={() => handleClick(i, j)} className="btn btn-neutral">{board[i][j].numMines}</div>);
         }
       }
     }
@@ -116,7 +126,45 @@ export default function MineSweeperPage() {
 
   function handleClick(row, col) {
     const newBoard = JSON.parse(JSON.stringify(board)); 
-    newBoard[row][col].revealed = true; 
+
+    const queue = [];
+
+    console.log("supp");
+    if (newBoard[row][col].numMines === 0) {
+      console.log("hi");
+      queue.push(newBoard[row][col]);
+    }
+
+    function addNeighbours(r, c, queue) {
+      if (r > 0 && newBoard[r-1][c].numMines === 0 && !newBoard[r-1][c].revealed) {
+        queue.push(newBoard[r-1][c]);
+      }
+
+      if (r < 9 && newBoard[r+1][c].numMines === 0 && !newBoard[r+1][c].revealed) {
+        queue.push(newBoard[r+1][c]);
+      }
+
+      if (c > 0 && newBoard[r][c - 1].numMines === 0 && !newBoard[r][c - 1].revealed) {
+        queue.push(newBoard[r][c - 1]);
+      }
+
+      if (c < 9 && newBoard[r][c + 1].numMines === 0 && !newBoard[r][c + 1].revealed) {
+        queue.push(newBoard[r][c + 1]);
+      }
+    }
+
+    while (queue.length > 0) {
+      const element = queue.shift();
+      
+      element.revealed = true;
+
+      const x = element.i;
+      const y = element.j;
+
+      addNeighbours(x, y, queue);
+    }
+    
+
     setBoard(newBoard);
   }
 
