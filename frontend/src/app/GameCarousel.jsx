96e57afe142159c189f1d40403e51c2a501b0b75
Guan Li <guanLi@student.unsw.edu.au>
2025-05-25 14:46:38 +1000

    added route

--- frontend/src/app/GameCarousel.jsx
+++ frontend/src/app/GameCarousel.jsx
@@ -25,10 +25,10 @@ export default function GameCarousel() {
       path: "/minesweeper"
     },
     {
-      title: "Desert Whispers",
+      title: "Clicker Game",
       button: "Play Game",
       src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      path: "/game4"
+      path: "/clicker"
     },
   ];
 
